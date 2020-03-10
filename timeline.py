d03742b034cfd9221df29fb457a75d424852334c
Martijn Arts <m.arts@gmx.net>
2020-03-10 23:59:08 +0100

    Update growth rate timeline. Add 3-day rolling median.

--- timeline.py
+++ timeline.py
@@ -1,25 +1,54 @@
 import plotly.graph_objects as go
 import dash_core_components as dcc
 
+PLOTLY_COLORS = ["#1f77b4", "#ff7f0e", "#2ca02c"]
+
+
+def _get_growth_rate_figure(df):
+    df = df / df.shift(1, axis=1)
+    df_smooth = df.rolling(3, axis=1).median()
+
+    raw_data = [
+        go.Scatter(
+            x=val.index, y=val, mode="markers", marker=dict(color=color), name=name
+        )
+        for (name, val), color in zip(df.iterrows(), PLOTLY_COLORS)
+        if val.any()
+    ]
+    smooth_data = [
+        go.Scatter(
+            x=val.index,
+            y=val,
+            mode="lines",
+            line=dict(color=color),
+            name=f"{name} (3-day median)",
+            hoverinfo="skip",
+        )
+        for (name, val), color in zip(df_smooth.iterrows(), PLOTLY_COLORS)
+        if val.any()
+    ]
+    return raw_data + smooth_data
+
 
 def plot_country_timeline(data, country, mode, yaxis):
     show_cols = ["Active", "Deaths", "Recovered"]
     df = data.get_country_data(country).loc[show_cols]
 
-    if mode == "difference":
-        df = df.diff(axis=1)
-        barmode = "relative"
-    elif mode == "growth factor":
-        df = df / df.shift(1, axis=1)
-        barmode = "group"
+    if mode == "growth factor":
+        fig_data = _get_growth_rate_figure(df)
+        barmode = None
     else:
-        barmode = "stack"
+        if mode == "difference":
+            df = df.diff(axis=1)
+            barmode = "relative"
+        else:
+            barmode = "stack"
 
-    fig_data = [
-        go.Bar(x=val.index, y=val, name=name)
-        for name, val in df.iterrows()
-        if val.any()
-    ]
+        fig_data = [
+            go.Bar(x=val.index, y=val, name=name)
+            for name, val in df.iterrows()
+            if val.any()
+        ]
 
     layout = dict(barmode=barmode, yaxis=dict(type=yaxis))
     fig = dict(data=fig_data, layout=layout)
